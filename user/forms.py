7168d060888365b8cb312ab6586573d29e3c9584
sunilkumar2020 <sunilrajputdev@gmail.com>
2021-05-23 22:43:10 +0530

    forgot password form create

--- user/forms.py
+++ user/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import widgets
+from django.forms.fields import EmailField
+
+class UserForgotPasswordForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control rounded-left", 
+            "type": "email", 
+            "placeholder": "Email"
+            }))
 
 class RegisterForm(UserCreationForm):
     password1 = forms.CharField(
